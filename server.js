e995b172c8b0a8f5d51f3b491d079e2274a77330
Nazir <hussainnazir80@gmail.com>
2021-01-22 11:52:51 +0530

    update server

---
+++ server.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser');
+
+const express = require('express');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', function(req, res){
+    res.send('Hello from Server');
+})
+
+app.post('/pay', function(req, res){
+    console.log(req.body);
+    res.status(200).send({"message": "Data received"});
+})
+
+app.listen(PORT, function(){
+    console.log("Server running on localhost: "+ PORT);
+});
